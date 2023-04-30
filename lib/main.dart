fcc66d186f1cfe9086ec66c2996e598e9b4d32be
LarsKhan <huzaifanawaz367@gmail.com>
2023-04-30 20:59:43 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:quiz_app/screens/serverside/Server.dart';
 import 'controllers/EventsController.dart';
 import 'controllers/TeamsController.dart';
 import 'controllers/question_controller.dart';
-import 'screens/serverside/result_Screen.dart';
+import 'screens/serverside/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,29 +19,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Quiz App',
-      onInit: () {
-        Get.put(TeamsController());
-        Get.put(EventController());
-        Get.put(QuestionController());
-        Get.put(Client());
-        Get.put(Server());
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(
-        useMaterial3: true,
-      ),
-      // home: AddMembersScreen(
-      //   event: eventss(
-      //     id: 0,
-      //     dates: 'djs',
-      //     type: 'sdsd',
-      //     status: 'ds',
-      //   ),
-      // ), // redesign  , list view of members (Done)
-      //home: const DashBoard() //pop up design on add event (Done)
-      //home: const AdminScreen(), //hide congratulation, mobile view, (Done)
-      home: const ResultScreen(score: 0), //UI Update (Done)
-    );
+        title: 'Quiz App',
+        onInit: () {
+          Get.put(TeamsController());
+          Get.put(EventController());
+          Get.put(QuestionController());
+          Get.put(Client());
+          Get.put(Server());
+        },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(
+          useMaterial3: true,
+        ),
+        // home: AddMembersScreen(
+        //   event: eventss(
+        //     id: 0,
+        //     dates: 'djs',
+        //     type: 'sdsd',
+        //     status: 'ds',
+        //   ),
+        // ), // redesign  , list view of members (Done)
+        home: const DashBoard() //pop up design on add event (Done)
+        //home: const AdminScreen(), //hide congratulation, mobile view, (Done)
+        //home: const ResultScreen(score: 0), //UI Update (Done)
+        );
   }
 }
